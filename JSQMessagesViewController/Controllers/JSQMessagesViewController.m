52140e41720bdb4797ea9565c9947969a867323b
Jesse Squires <jesse.d.squires@gmail.com>
2016-05-30 15:48:19 -0700

    remove old iOS 7 hacks

--- JSQMessagesViewController/Controllers/JSQMessagesViewController.m
+++ JSQMessagesViewController/Controllers/JSQMessagesViewController.m
@@ -36,7 +36,6 @@
 
 #import "NSString+JSQMessages.h"
 #import "UIColor+JSQMessages.h"
-#import "UIDevice+JSQMessages.h"
 #import "NSBundle+JSQMessages.h"
 
 #import <MobileCoreServices/UTCoreTypes.h>
@@ -564,14 +563,6 @@ JSQMessagesKeyboardControllerDelegate>
 
     if (!isMediaMessage) {
         cell.textView.text = [messageItem text];
-
-        if ([UIDevice jsq_isCurrentDeviceBeforeiOS8]) {
-            //  workaround for iOS 7 textView data detectors bug
-            cell.textView.text = nil;
-            cell.textView.attributedText = [[NSAttributedString alloc] initWithString:[messageItem text]
-                                                                           attributes:@{ NSFontAttributeName : collectionView.collectionViewLayout.messageBubbleFont }];
-        }
-
         NSParameterAssert(cell.textView.text != nil);
 
         id<JSQMessageBubbleImageDataSource> bubbleImageDataSource = [collectionView.dataSource collectionView:collectionView messageBubbleImageDataForItemAtIndexPath:indexPath];
