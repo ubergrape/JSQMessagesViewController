55c5a0b3ccdb9367ac952cb3ac7ad03279fd9eac
Lukas Gauster <lukas.gauster@gmail.com>
2018-03-02 14:06:51 +0100

    renamed pastdelegate to fix ios10 crash

--- JSQMessagesViewController/Views/JSQMessagesComposerTextView.h
+++ JSQMessagesViewController/Views/JSQMessagesComposerTextView.h
@@ -55,7 +55,7 @@
 /**
  *  The object that acts as the paste delegate of the text view.
  */
-@property (weak, nonatomic) id<JSQMessagesComposerTextViewPasteDelegate> pasteDelegate;
+@property (weak, nonatomic) id<JSQMessagesComposerTextViewPasteDelegate> JSQPasteDelegate;
 
 /**
  *  Determines whether or not the text view contains text after trimming white space 
--- JSQMessagesViewController/Views/JSQMessagesComposerTextView.m
+++ JSQMessagesViewController/Views/JSQMessagesComposerTextView.m
@@ -142,7 +142,7 @@
 
 - (void)paste:(id)sender
 {
-    if (!self.pasteDelegate || [self.pasteDelegate composerTextView:self shouldPasteWithSender:sender]) {
+    if (!self.JSQPasteDelegate || [self.JSQPasteDelegate composerTextView:self shouldPasteWithSender:sender]) {
         [super paste:sender];
     }
 }
